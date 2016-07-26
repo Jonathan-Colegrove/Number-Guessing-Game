0a30fd5eb2f0daa8e0ac81797c0a659a165a12c5
Jonathan Colegrove <Demetrius472@gmail.com>
2016-07-26 14:25:33 -0400

    if number too low, prompt to try again

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,6 +1,19 @@
 # Pick a random number between 1 and 100
-rand(1..100)
+random = rand(1..100)
 
 # Prompt you for a guess
-puts "Guess a # between 1 - 100"
-answer = gets.chomp.to_i
+def guess
+  puts "Guess a # between 1 - 100"
+
+  return gets.chomp.to_i
+end
+
+puts "CHEAT MODE: REMOVE THIS BEFORE SHIPPING OUR GAME: #{random}"
+
+loop do
+  # If your guess is less than the program's number, it should tell you that you were low and let you guess again
+  answer = guess
+  if answer < random
+    puts "Oops!  A bit too low, try again!"
+  end
+end
